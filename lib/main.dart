e3d170507ccab2a2d761a00cb8cf9193c6b428da
Ming Lyu (CareF) <minglyu@google.com>
2020-06-16 16:47:49 -0400

    Add comparison

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,63 @@ void main() {
 }
 
 class ComplexLayoutApp extends StatelessWidget {
+  Widget _tileScrollLayout(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Tile Scrolling Layout')),
+      body: ListView.builder(
+        key: const Key('tiles-scroll'),
+        itemCount: 200,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Material(
+              elevation: (index % 5 + 1).toDouble(),
+              child: IconBar(),
+            ),
+          );
+        },
+      ),
+      drawer: const SettingDrawer(),
+    );
+  }
+
+  Widget _complexLayout(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Advanced Layout'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.create),
+            tooltip: 'Search',
+            onPressed: () {
+              print('Pressed search');
+            },
+          ),
+          TopBarMenu(),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              // this key is used by the driver test
+              key: const Key('complex-scroll'),
+              itemBuilder: (BuildContext context, int index) {
+                if (index % 2 == 0)
+                  return FancyImageItem(index, key: PageStorageKey<int>(index));
+                else
+                  return FancyGalleryItem(index,
+                      key: PageStorageKey<int>(index));
+              },
+            ),
+          ),
+          BottomBar(),
+        ],
+      ),
+      drawer: const SettingDrawer(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ModelBinding<SettingConfig>(
@@ -33,6 +90,8 @@ class ComplexLayoutApp extends StatelessWidget {
             home: scrollMode == ScrollMode.complex
                 ? const ComplexLayout()
                 : const TileScrollLayout(),
+//                  ? _complexLayout(context)
+//                  : _tileScrollLayout(context),
           );
         },
       ),
